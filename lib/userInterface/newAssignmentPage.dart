cd018c29ea94af44af7fee0a34f14d402100621f
Marcelxy <quiz.match20@gmail.com>
2020-06-26 20:34:08 +0200

    Neuer Auftrag Seite
    - Neuer Auftrag Seite angefangen zu implementieren.

--- lib/userInterface/newAssignmentPage.dart
+++ lib/userInterface/newAssignmentPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class NewAssignmentPage extends StatefulWidget {
   @override
@@ -6,6 +7,42 @@ class NewAssignmentPage extends StatefulWidget {
 }
 
 class _NewAssignmentPageState extends State<NewAssignmentPage> {
+  DateTime _date;
+  int currentStep = 0;
+  bool complete = false;
+  String _currentOrderType;
+  StepperType stepperType = StepperType.horizontal;
+  TextEditingController _consumerName = TextEditingController();
+  CalendarController _calendarController = CalendarController();
+  List<DropdownMenuItem<String>> _dropdownMenuOrderType;
+  List<String> _orderType = [
+    'Holzfenster IV 68',
+    'Holzfenster IV 78',
+    'Holzfenster IV 88',
+    'Holz Alu Fenster IV 68',
+    'Holz Alu Fenster IV 78',
+    'Holz Alu Fenster IV 88',
+    'Haustüre',
+    'Pfosten Riegel',
+    'Leisten',
+    'Sonstiges'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _dropdownMenuOrderType = getDropdownMenuItemsForOrderType();
+    _currentOrderType = _dropdownMenuOrderType[0].value;
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _consumerName.dispose();
+    _calendarController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +50,157 @@ class _NewAssignmentPageState extends State<NewAssignmentPage> {
         title: Text('Neuer Auftrag'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stepper(
+              steps: [
+                Step(
+                  title: Text('Name des Kunden: ${_consumerName.text.toString()}'),
+                  isActive: true,
+                  state: StepState.indexed,
+                  content: Column(
+                    children: [
+                      TextFormField(
+                        controller: _consumerName,
+                        decoration: InputDecoration(
+                          labelText: 'Kundenname',
+                          prefixIcon: Icon(Icons.person, size: 22.0),
+                          contentPadding: const EdgeInsets.all(0),
+                          isDense: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Step(
+                  title: const Text('Auftragsart: '),
+                  isActive: true,
+                  state: StepState.indexed,
+                  content: Column(
+                    children: [
+                      DropdownButton(
+                        value: _currentOrderType,
+                        items: _dropdownMenuOrderType,
+                        onChanged: changedDropdownOrderType,
+                      ),
+                    ],
+                  ),
+                ),
+                Step(
+                  title: const Text('Anzahl Elemente: '),
+                  isActive: true,
+                  state: StepState.indexed,
+                  content: Column(
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(labelText: 'Anzahl Elemente'),
+                      ),
+                    ],
+                  ),
+                ),
+                Step(
+                  title: Text('Einbautermin: ${_date == null ? '' : _date.toString()}'),  // TODO hier weitermachen intl einbauen für Datumsformat
+                  isActive: true,
+                  state: StepState.indexed,
+                  content: Column(
+                    children: [
+                      TableCalendar(
+                        calendarController: _calendarController,
+                        locale: 'en_US',
+                        startingDayOfWeek: StartingDayOfWeek.monday,
+                        availableCalendarFormats: const {
+                          CalendarFormat.month: 'Month',
+                        },
+                        daysOfWeekStyle: DaysOfWeekStyle(
+                          weekendStyle: TextStyle(
+                            color: Colors.green.shade700,
+                          ),
+                        ),
+                        onDaySelected: (date, events) {
+                          print(date.toUtc());
+                          _date = date.toUtc();
+                        },
+                        calendarStyle: CalendarStyle(
+                          todayColor: Theme.of(context).backgroundColor,
+                          todayStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                          weekendStyle: TextStyle(
+                            color: Colors.green.shade700,
+                          ),
+                          outsideWeekendStyle: TextStyle(
+                            color: Colors.green.shade700,
+                          ),
+                          selectedColor: Colors.blueAccent,
+                          selectedStyle: TextStyle(
+                            fontSize: 17.0,
+                          ),
+                        ),
+                        headerStyle: HeaderStyle(
+                          formatButtonVisible: false,
+                          centerHeaderTitle: true,
+                          leftChevronIcon: Icon(Icons.chevron_left, color: Colors.white),
+                          rightChevronIcon: Icon(Icons.chevron_right, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Step(
+                  title: const Text('Priorität: '),
+                  isActive: true,
+                  state: StepState.indexed,
+                  content: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Priorität'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+              currentStep: currentStep,
+              onStepContinue: next,
+              onStepCancel: cancel,
+              onStepTapped: (step) => goTo(step),
+            ),
+          ),
+          RaisedButton(
+            onPressed: () => {},
+            child: Text('Erstellen'),
+          ),
+        ],
+      ),
     );
+  }
+
+  next() {
+    currentStep + 1 != 5 ? goTo(currentStep + 1) : setState(() => complete = true);
+  }
+
+  cancel() {
+    if (currentStep > 0) {
+      goTo(currentStep - 1);
+    }
+  }
+
+  goTo(int step) {
+    setState(() => currentStep = step);
+  }
+
+  List<DropdownMenuItem<String>> getDropdownMenuItemsForOrderType() {
+    List<DropdownMenuItem<String>> items = new List();
+    for (String orderType in _orderType) {
+      items.add(new DropdownMenuItem(value: orderType, child: new Text(orderType)));
+    }
+    return items;
+  }
+
+  void changedDropdownOrderType(String selectedChoice) {
+    setState(() {
+      _currentOrderType = selectedChoice;
+    });
   }
 }
