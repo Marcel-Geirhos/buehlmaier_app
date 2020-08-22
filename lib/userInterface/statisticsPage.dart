65ec286ca896d7be0161325bbb710afbc7f036d6
Marcelxy <quiz.match20@gmail.com>
2020-08-22 13:52:22 +0200

    Statistik
    - Statistik weiter implementiert.

--- lib/userInterface/statisticsPage.dart
+++ lib/userInterface/statisticsPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:buehlmaier_app/utils/chart.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:buehlmaier_app/models/workload.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:buehlmaier_app/utils/systemSettings.dart';
 
 class StatisticsPage extends StatefulWidget {
@@ -9,12 +12,21 @@ class StatisticsPage extends StatefulWidget {
 }
 
 class _StatisticsPageState extends State<StatisticsPage> {
+  int _numberYears;
+  List<int> _numberOfDoors = [];
+  List<int> _numberOfPosts = [];
+  List<int> _numberOfWindows = [];
+  List<Workload> data = [];
   QuerySnapshot _assignmentStatistics;
 
   @override
   void initState() {
     super.initState();
     SystemSettings.allowOnlyPortraitOrientation();
+    _numberYears = 0;
+    /*_numberOfDoors = 0;
+    _numberOfPosts = 0;
+    _numberOfWindows = 0;*/
   }
 
   @override
@@ -29,13 +41,12 @@ class _StatisticsPageState extends State<StatisticsPage> {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             return Swiper(
-              itemCount: 3,
+              itemCount: _numberYears,
+              scale: 0.9,
+              viewportFraction: 0.8,
+              loop: false,
               itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: Column(
-                    children: <Widget>[],
-                  ),
-                );
+                return cardList(index);
               },
             );
           }
@@ -45,9 +56,73 @@ class _StatisticsPageState extends State<StatisticsPage> {
     );
   }
 
+  Widget cardList(int index) {
+    int currentYear = index + 2019; // TODO auf 2020 ändern!
+    return ListView(
+      children: <Widget>[
+        Card(
+          elevation: 8.0,
+          margin: EdgeInsets.fromLTRB(5, 100, 5, 80),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Text('$currentYear', style: TextStyle(fontSize: 32.0)),
+              ),
+              StatisticsChart(data: data, index: index),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
   Future<void> loadAssignmentStatistics() async {
-    for (int year = 2020; year <= DateTime.now().year; year++) {
+    int chartNumber = 0;
+    int doors = 0;
+    int posts = 0;
+    int windows = 0;
+    // TODO auf 2020 ändern!
+    for (int year = 2019; year <= DateTime.now().year; year++, _numberYears++, chartNumber++) {
       _assignmentStatistics = await Firestore.instance.collection('archive_$year').getDocuments();
+      for (int i = 0; i < _assignmentStatistics.documents.length; i++) {
+        int numberOfElements = int.parse(_assignmentStatistics.documents[i].data['NumberOfElements']);
+        String orderType = _assignmentStatistics.documents[i].data['OrderType'];
+        if (orderType == 'Haustüre') {
+          doors += numberOfElements;
+        } else if (orderType == 'Pfosten Riegel') {
+          posts += numberOfElements;
+        } else if (orderType == 'Leisten' || orderType == 'Sonstiges') {
+          // Wird nicht erfasst.
+        } else {
+          windows += numberOfElements;
+        }
+      }
+      _numberOfDoors.add(doors);
+      _numberOfPosts.add(posts);
+      _numberOfWindows.add(windows);
+      addChartData(chartNumber);
+    }
+  }
+
+  void addChartData(int index) {
+    data.add(Workload(
+      name: 'Türen',
+      count: _numberOfDoors[index],
+      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+    ));
+    data.add(Workload(
+      name: 'Fenster',
+      count: _numberOfWindows[index],
+      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+    ));
+    data.add(Workload(
+      name: 'Pfosten',
+      count: _numberOfPosts[index],
+      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+    ));
+    for (int i = 0; i < data.length; i++) {
+      print(data[i].count);
     }
   }
 }
--- lib/utils/chart.dart
+++ lib/utils/chart.dart
@@ -64,3 +64,68 @@ class WorkloadChart extends StatelessWidget {
     );
   }
 }
+
+class StatisticsChart extends StatelessWidget {
+  final List<Workload> data;
+  final int index;
+
+  StatisticsChart({@required this.data, @required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    print(data[0].count);
+    List<charts.Series<Workload, String>> series = [
+      charts.Series(
+          id: "Workload",
+          data: data,
+          seriesColor: charts.ColorUtil.fromDartColor(Colors.white),
+          domainFn: (Workload series, _) => series.name,
+          measureFn: (Workload series, _) => series.count,
+          colorFn: (Workload series, _) => series.barColor),
+    ];
+    return Container(
+      height: 400,
+      padding: const EdgeInsets.all(20),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                'Jahr',
+                style: TextStyle(fontSize: 28.0, letterSpacing: 1.2),
+              ),
+              Expanded(
+                child: charts.BarChart(
+                  series,
+                  animate: true,
+                  animationDuration: Duration(milliseconds: 1200),
+                  domainAxis: new charts.OrdinalAxisSpec(
+                    renderSpec: new charts.SmallTickRendererSpec(
+                      // Tick and Label styling here.
+                      labelStyle: new charts.TextStyleSpec(
+                          fontSize: 16, // size in Pts.
+                          color: charts.MaterialPalette.white),
+                      // Change the line colors to match text color.
+                      lineStyle: new charts.LineStyleSpec(color: charts.MaterialPalette.white),
+                    ),
+                  ),
+                  primaryMeasureAxis: new charts.NumericAxisSpec(
+                    renderSpec: new charts.GridlineRendererSpec(
+                      // Tick and Label styling here.
+                      labelStyle: new charts.TextStyleSpec(
+                          fontSize: 16, // size in Pts.
+                          color: charts.MaterialPalette.white),
+                      // Change the line colors to match text color.
+                      lineStyle: new charts.LineStyleSpec(color: charts.MaterialPalette.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
